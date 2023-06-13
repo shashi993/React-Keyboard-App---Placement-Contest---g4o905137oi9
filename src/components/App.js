c0110d1cac6013cd9a62d7c99c360f0849abffa6
shashi993 <shashi.raj213@gmail.com>
2023-06-13 22:41:21 +0530

    appjs

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,43 @@ import React, { useState, useEffect } from "react";
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
+  const [value, setValue] = useState('');
+  const [data, setData] = useState('');
+
+  function handleClick(key) {
+    setValue(prevValue => prevValue + key);
+  }
+
+  useEffect(() => {
+    function fetchData() {
+      fetch('https://api.quotable.io/random')
+        .then(res => res.json())
+        .then(data => setData(data.content));
+    }
+    fetchData();
+  }, []);
 
   return (
-    <div className="keyboard">
-      <div className="preview"></div>
-      <div>
-        {keys.map((key) => (
-          <button key={key} id={key === " " ? `key-space` : `key-${key}`}>
-            {key === " " ? "Space" : key.toUpperCase()}
-          </button>
-        ))}
-      </div>
-    </div>
+    <>
+      {value === "forty two" ? (
+        <div className="quote">{data}</div>
+      ) : (
+        <div className="keyboard">
+          <div className="preview">{value}</div>
+          <div>
+            {keys.map((key) => (
+              <button
+                onClick={() => handleClick(key)}
+                key={key}
+                id={key === " " ? `key-space` : `key-${key}`}
+              >
+                {key === " " ? "Space" : key.toUpperCase()}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
